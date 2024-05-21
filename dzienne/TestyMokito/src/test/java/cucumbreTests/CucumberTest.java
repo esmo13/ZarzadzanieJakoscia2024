a84e15d404950525d5142f1b0b3f5dc49a19e1eb
kpodlaski <podlaski@uni.lodz.pl>
2024-05-21 11:45:10 +0200

    cucumbre start

--- dzienne/TestyMokito/src/test/java/cucumbreTests/CucumberTest.java
+++ dzienne/TestyMokito/src/test/java/cucumbreTests/CucumberTest.java
@@ -1,9 +1,6 @@
 package cucumbreTests;
 import io.cucumber.junit.platform.engine.Constants;
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 //@CucumberOptions(
 //            //plugin = {"pretty"},
@@ -12,10 +9,10 @@ import org.junit.platform.suite.api.Suite;
 //    )
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("cucumbreTests")
+@SelectPackages("cucumbreTests")
 @ConfigurationParameter(
-        key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/features/MoneyTransfer.feature")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "cucumbreTests")
+        key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/features")
+//@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "src/test/resources/features")
 //@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@googleSearch")
 //@ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
 //@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
